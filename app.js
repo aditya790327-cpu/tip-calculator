aac5ec141c390ffad87bf5db00801994b4e57ec0
Aditya Choudhary <adityachoudhary@Adityas-MacBook-Pro-3.local>
2025-12-12 20:01:45 +0530

    website

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 
+function calculateTip() {
+    let bill = parseFloat(document.getElementById("bill").value);
+    let tip = parseFloat(document.getElementById("tip").value);
+    let people = parseInt(document.getElementById("people").value);
+
+    // default values if empty
+    if (isNaN(bill)) bill = 0;
+    if (isNaN(tip)) tip = 0;
+    if (isNaN(people) || people <= 0) people = 1;
+
+    let tipAmount = (bill * (tip / 100)) / people;
+    let totalAmount = (bill / people) + tipAmount;
+
+    document.getElementById("tipPerPerson").innerText = tipAmount.toFixed(2);
+    document.getElementById("totalPerPerson").innerText = totalAmount.toFixed(2);
+}
